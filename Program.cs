0c2c8e7d4b1b21d0bfe3fdf1fc589eaa2b6a0094
Owen Kitchener <okitchener@my.wctc.edu>
2025-09-30 14:17:15 -0500

    [1.12a] Remove Character

--- Program.cs
+++ Program.cs
@@ -50,6 +50,15 @@ do
     else if (choice == "3")
     {
         // Remove Mario Character
+        Console.WriteLine("Enter Id of the Character to remove:");
+        if (UInt32.TryParse(Console.ReadLine(), out UInt32 Id))
+        {
+            logger.Info($"Character Id {Id} entered");
+        }
+        else
+        {
+            logger.Error("Invalid Id");
+        }
     }
     else if (string.IsNullOrEmpty(choice))
     {
