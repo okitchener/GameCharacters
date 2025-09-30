97fffbd30c863e09806a62346f0029dae69521e7
Owen Kitchener <okitchener@my.wctc.edu>
2025-09-30 14:02:50 -0500

    [1.11c] Add Character - Alias

--- Program.cs
+++ Program.cs
@@ -44,6 +44,19 @@ do
         mario.Name = Console.ReadLine();
         Console.Write("Enter Description: ");
         mario.Description = Console.ReadLine();
+        // Input Alias
+        List<string> list = [];
+        do
+        {
+            Console.WriteLine($"Enter Alias or (enter) to quit:");
+            string response = Console.ReadLine()!;
+            if (string.IsNullOrEmpty(response))
+            {
+                break;
+            }
+            list.Add(response);
+        } while (true);
+        mario.Alias = list;
     }
     else if (choice == "3")
     {
