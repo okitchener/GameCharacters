0ed39c678f6a55c9a2aa2b08ab84552661fac083
Owen Kitchener <okitchener@my.wctc.edu>
2025-09-30 14:13:14 -0500

    [1.11e] Refactor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using System.Reflection;
 using System.Text.Json;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
@@ -39,24 +40,7 @@ do
         {
             Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
         };
-        // Input Name, Description
-        Console.Write("Enter Name: ");
-        mario.Name = Console.ReadLine();
-        Console.Write("Enter Description: ");
-        mario.Description = Console.ReadLine();
-        // Input Alias
-        List<string> list = [];
-        do
-        {
-            Console.WriteLine($"Enter Alias or (enter) to quit:");
-            string response = Console.ReadLine()!;
-            if (string.IsNullOrEmpty(response))
-            {
-                break;
-            }
-            list.Add(response);
-        } while (true);
-        mario.Alias = list;
+        InputCharacter(mario);
         // Add Character
         marios.Add(mario);
         File.WriteAllText(marioFileName,
@@ -78,3 +62,33 @@ do
 } while (true);
 
 logger.Info("Program ended");
+
+static void InputCharacter(Character character)
+{
+    Type type = character.GetType();
+    PropertyInfo[] properties = type.GetProperties();
+    var props = properties.Where(p => p.Name != "Id");
+    foreach (PropertyInfo prop in props)
+    {
+        if (prop.PropertyType == typeof(string))
+        {
+            Console.WriteLine($"Enter {prop.Name}:");
+            prop.SetValue(character, Console.ReadLine());
+        }
+        else if (prop.PropertyType == typeof(List<string>))
+        {
+            List<string> list = [];
+            do
+            {
+                Console.WriteLine($"Enter {prop.Name} or (Enter to quit):");
+                string response = Console.ReadLine()!;
+                if (string.IsNullOrEmpty(response))
+                {
+                    break;
+                }
+            list.Add(response);
+            } while (true);
+            prop.SetValue(character, list);
+        }
+    }
+}
