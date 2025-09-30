3f2246b92bc4568a40cfc88067f975a6921b1913
Owen Kitchener <okitchener@my.wctc.edu>
2025-09-30 14:00:23 -0500

    [1.11b] Add Character - Input Name, Description

--- Program.cs
+++ Program.cs
@@ -39,6 +39,11 @@ do
         {
             Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
         };
+        // Input Name, Description
+        Console.Write("Enter Name: ");
+        mario.Name = Console.ReadLine();
+        Console.Write("Enter Description: ");
+        mario.Description = Console.ReadLine();
     }
     else if (choice == "3")
     {
