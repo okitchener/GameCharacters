21bfaa50f8c77a44a0c79dc4f8edc5d8258c6636
Owen Kitchener <okitchener@my.wctc.edu>
2025-09-30 13:56:39 -0500

    [1.09] Create menu of user choices

--- Program.cs
+++ Program.cs
@@ -12,4 +12,34 @@ string marioFileName = "mario.json";
 List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>
 (File.ReadAllText(marioFileName))!;
 
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Display Mario Characters");
+  Console.WriteLine("2) Add Mario Character");
+  Console.WriteLine("3) Remove Mario Character");
+  Console.WriteLine("Enter to quit");
+
+  // input selection
+  string? choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+
+  if (choice == "1")
+  {
+    // Display Mario Characters
+  }
+  else if (choice == "2")
+  {
+    // Add Mario Character
+  }
+  else if (choice == "3")
+  {
+    // Remove Mario Character
+  } else if (string.IsNullOrEmpty(choice)) {
+    break;
+  } else {
+    logger.Info("Invalid choice");
+  }
+} while (true);
+
 logger.Info("Program ended");
